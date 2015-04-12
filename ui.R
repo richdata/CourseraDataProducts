4369eb6026520f651c6d027b98690d9a45a30b82
Rich Sodon <rich.sodon@verizon.com>
2015-04-12 01:05:51 -0400

    Added Explanatory Commentary
    Added  documentation so that a novice user could use the application.

--- ui.R
+++ ui.R
@@ -14,7 +14,30 @@ shinyUI(pageWithSidebar(
                                      "*" = "Times", 
                                      "/" = "Divided by",
                                      "^" = "To The Power Of"), inline = TRUE),
-                numericInput('id3', 'Enter second number',0,min=0, max=10000, step=0.5)
+                numericInput('id3', 'Enter second number',0,min=0, max=10000, step=0.5),
+ #
+ #
+ #
+                h4("Explanatory text"),
+
+ 
+                p("This is simple calculator app.  As you enter two numbers and
+                  pick an operation, the app performs the calculation in real time",
+                  style = "font-family: 'baskerville'; font-si16pt"), 
+ 
+                 p("Using this app - You enter a number in the first edit box, 
+                   select the operation you would like to perform with the radio buttons
+                   and enter the second number of your calculation.  As you make each
+                  entry, the app will update the calculation to the right.",
+                 style = "font-family: 'baskerville'; font-si16pt"),
+			
+		p("People who might be interested in seeing how I did this
+                  can review the code here:", 
+			  style = "font-family: 'baskerville'; font-si16pt"),
+			
+			a(href = "https://github.com/richdata/CourseraDataProducts",
+			  "Link to source code on GitHub")
+                
                 ),
 #
 #
@@ -23,10 +46,10 @@ shinyUI(pageWithSidebar(
 #
         mainPanel
                 (h3("Showing your work"),
-                 h4('This first number'),
+                 h4('This first number,'),
                  verbatimTextOutput("oid1"),
                  verbatimTextOutput("oid2"),
-                 h4('This second number'),
+                 h4('This second number,'),
                  verbatimTextOutput('oid3'),
                  h4('EQUALS'),
                  verbatimTextOutput('oid4')
